5e5a1dd98e5cf75e75825cd8da09fcf9d4adc622
Molly <mollymccollumwx@gmail.com>
2021-01-08 15:27:10 -0500

    added markdown to generate markdown.js, added list to license input, added default to contributors input, added MIT license and badge

--- index.js
+++ index.js
@@ -1,64 +1,78 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-
-// TODO: Create an array of questions for user input
+// Array of questions for user input
 const questions = [
-    {
-        type: "input",
-        message: "What is the title of your project?:",
-        name: "title",
-    },
-    {
-        type: "input",
-        message: "Please enter a short description of your project:",
-        name: "description",
-    },
-    {
-        type: "input",
-        message: "Please enter installation instructions for you project:",
-        name: "installation",
-    },
-    {
-        type: "input",
-        message: "Please enter how to use your project",
-        name: "usage",
-    },
-    {
-        type: "input",
-        message: "Please enter your project title:",
-        name: "contributing",
-    },
-    {
-        type: "input",
-        message: "Please enter your project title:",
-        name: "test",
-    }
-    // {
-    //     type: "list",
-    //     message: "Please enter your project title:",
-    //     name: "license",
-    // }
-    
-    
+  {
+    type: "input",
+    message: "What is the title of your project?:",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Please enter a short description of your project:",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Please enter installation instructions for your project:",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Please enter instructions for using your project:",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Please enter the names of any contributors to your project:",
+    name: "contributing",
+    default:
+      "Please see the Questions section below to contact the author to be added as a contributor.",
+  },
+  {
+    type: "input",
+    message: "Please enter instructions on how to run tests on your project:",
+    name: "test",
+  },
+  {
+    type: "list",
+    message: "Please select a license for your project",
+    name: "license",
+    choices: [
+      "MIT License",
+      "GNU GPL 3.0",
+      "Apache License 2.0",
+      "ISC License",
+    ],
+  },
+  {
+    type: "input",
+    message: "What is your Github username:",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "What is your email?:",
+    name: "email",
+  },
 ];
 
-// TODO: Create a function to write README file
+// Function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, generateMarkdown(data), (err)=>{
-        err ? console.error(err) : console.log("Success!")
-    });
+  fs.writeFile(fileName, generateMarkdown(data), (err) => {
+    err ? console.error(err) : console.log("Success!");
+  });
 }
 
-// TODO: Create a function to initialize app
+// Function to initialize app
 function init() {
-    console.log("the function has run");
-    inquirer.prompt(questions).then((response) => {
-        console.log(response);
-        writeToFile("generatedREADME.md", response);
-    });
+  inquirer.prompt(questions).then((response) => {
+    console.log(response);
+    writeToFile("generatedREADME.md", response);
+  });
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,93 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "MIT License"){
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "MIT License"){
+    return `MIT License
+
+    Copyright (c) [2020]
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.`
+  } else if (license === "GNU GPL 3.0"){
+    return `GNU GENERAL PUBLIC LICENSE
+    Version 3, 29 June 2007
+
+Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+Everyone is permitted to copy and distribute verbatim copies
+of this license document, but changing it is not allowed.`
+  } else {
+    return ""
+  } 
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-  
+  return `
+# ${data.title}
+${renderLicenseBadge(data.license)}
 
+## Description
+${data.description}
+
+
+## Table of Contents
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [License](#License)
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [Questions](#Questions)
+
+## Installation
+${data.installation}
+
+## Usage 
+${data.usage}
+
+## License
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+
+## Tests
+${data.test}
+
+## Questions
+If you have any questions, feel free to contact me on [Github](https://www.github.com/${data.github}).
+Alternatively, you can email me at ${data.email}.
+  
 `;
 }
 
