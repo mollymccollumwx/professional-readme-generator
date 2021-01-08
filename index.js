7a35c537a983d5938a10847ec8d4cf4d9e623936
Molly <mollymccollumwx@gmail.com>
2021-01-07 22:06:44 -0500

    added first 4 questions in index.js

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 // TODO: Include packages needed for this application
+//fs and inquirer 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "Please enter your project title:",
+        name: "projectTitle"
+    },
+    {   type: "input",
+        message: "Please enter a description of your project:",
+        name: "projectDescription"
+    },
+    {
+        type: "input",
+        message: "Please enter instructions for installation",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Please enter usage information",
+        name: "usage"
+    },
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() => {
+    promptUser();
+}
 
 // Function call to initialize app
 init();
