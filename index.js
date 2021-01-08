028dfb0bfb9ef9510745df78829a1f573787c2b8
Molly <mollymccollumwx@gmail.com>
2021-01-08 12:31:54 -0500

    tested one question with completed init and writeToFile functions

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
 // TODO: Include packages needed for this application
-//fs and inquirer 
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: "input",
         message: "Please enter your project title:",
-        name: "projectTitle"
-    },
-    {   type: "input",
-        message: "Please enter a description of your project:",
-        name: "projectDescription"
-    },
-    {
-        type: "input",
-        message: "Please enter instructions for installation",
-        name: "installation"
-    },
-    {
-        type: "input",
-        message: "Please enter usage information",
-        name: "usage"
-    },
+        name: "title",
+    }
     
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), (err)=>{
+        err ? console.error(err) : console.log("Success!")
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() => {
-    promptUser();
+function init() {
+    console.log("the function has run");
+    inquirer.prompt(questions).then((response) => {
+        console.log(response);
+        writeToFile("generatedREADME.md", response);
+    });
 }
 
 // Function call to initialize app
